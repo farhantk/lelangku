b9d6f720eb78118ef95a17018e28c4d123ea0c2f
FARHAN TIRTA KESUMAH <tirtafarhan@student.telkomuniversity.ac.id>
2022-11-28 20:01:31 +0700

    Update router.js

--- app/user/router.js
+++ app/user/router.js
@@ -1,13 +1,6 @@
 var express = require('express');
 var router = express.Router();
-<<<<<<< Updated upstream
-const { index, editProfile } = require('./controller')
-const { isLoginUser } = require('../middleware/auth')
 
-/* GET home page. */
-router.get('/', isLoginUser, index);
-router.put('/', isLoginUser, editProfile);
-=======
 const { editProfile, index } = require('./controller')
 const { isLoginUser } = require('../middleware/auth')
 
@@ -15,6 +8,5 @@ const { isLoginUser } = require('../middleware/auth')
 
 router.get('/', isLoginUser, index);
 router.put('/', isLoginUser, editProfile);
->>>>>>> Stashed changes
 
 module.exports = router;
