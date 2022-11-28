2314100c248c9ad2ea32a6dfd6fb8ae81b58cf53
FARHAN TIRTA KESUMAH <tirtafarhan@student.telkomuniversity.ac.id>
2022-11-28 20:28:01 +0700

    Update router.js

--- app/user/router.js
+++ app/user/router.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var router = express.Router();
-const { editProfile, view_editProfile } = require('./controller')
+const { editProfile, index } = require('./controller')
 const { isLoginUser } = require('../middleware/auth')
 
 /* GET home page. */
 
-router.get('/profile', isLoginUser, view_editProfile);
-router.put('/profile', isLoginUser, editProfile);
+router.get('/', isLoginUser, index);
+router.put('/', isLoginUser, editProfile);
 
 module.exports = router;
