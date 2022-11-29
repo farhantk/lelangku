7607bed22f25506ac31e80a7e67e1a9d92ccd457
FARHAN TIRTA KESUMAH <tirtafarhan@student.telkomuniversity.ac.id>
2022-11-29 11:28:55 +0700

    bug integrate topup

--- app/topup/controller.js
+++ app/topup/controller.js
@@ -4,9 +4,11 @@ module.exports={
     index: async(req, res)=>{
         User.findOne({_id: req.session.user.id}, (err, result) => {
             try {
+                res.render('client/TopUp/index')
                 res.status(200).json({
                     data: result
                 })
+                res.redirect('/')
                 
             } catch (err) {
                 console.log(err)
@@ -15,17 +17,20 @@ module.exports={
     },
     topup: async(req, res)=>{
         var addbalance =  req.body
-        //const temp = parseInt(Object.values(addbalance))
-        const temp = parseInt(addbalance.balance)
+        console.log(addbalance)
+        const temp = parseInt(Object.values(addbalance))
+        //const temp = parseInt(addbalance)
+        console.log(temp)
         User.findOneAndUpdate({
             _id: req.session.user.id
         }, {$inc:{balance:temp}}, (err, result)=>{
             try {
+                res.redirect('/')
                 res.status(200).json({
                     data:result
                 })
             } catch (error) {
-                console.log(error)
+                res.redirect('/')
             }
         })
         
