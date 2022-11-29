1fd6f45e7925b80e8141963804ec4cd7b493df34
SyamDN <76995702+SyamDN@users.noreply.github.com>
2022-11-29 12:30:16 +0700

    Add js
    Penambahan javascript

---
+++ public/javascripts/js.js
@@ -0,0 +1,11 @@
+function kategoryOn() {
+  var kategoryy = document.getElementById('kategory');
+
+  kategoryy.classList.toggle('kategoryStyle');
+}
+
+let subMenu = document.getElementById('subMenu');
+
+function toggleMenu() {
+  subMenu.classList.toggle('open-menu');
+}
